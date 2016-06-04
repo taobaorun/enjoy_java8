a8ce3dea6840306ffd96306870c04413b759ea69
wutao <taobaorun@gmail.com>
2016-06-04 16:41:30 +0800

    init

---
+++ src/main/java/com/jiaxy/enjoyjava8/Student.java
@@ -0,0 +1,15 @@
+package com.jiaxy.enjoyjava8;
+
+/**
+ * Title: <br>
+ * <p>
+ * Description: <br>
+ * </p>
+ * <br>
+ *
+ * @author <a href=mailto:taobaorun@gmail.com>taobaorun</a>
+ *         <br>
+ * @since 2016/06/04 16:39
+ */
+public class Student {
+}
